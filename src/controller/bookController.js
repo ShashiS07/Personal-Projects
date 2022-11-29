5fdd1b0a5c036ff117b03994c61958d8bf3a72f3
palsubodh <palsubodh390@gmail.com>
2022-11-29 16:30:19 +0530

    done

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -8,29 +8,57 @@ const{isValidObjectId}=require("mongoose")
 // ==============================create books===========================================
 
 const createBook = async function(req, res){
-    try{
-        let reqBody = req.body
-        const {title, excerpt, userId, ISBN, category, subcategory, releasedAt } = reqBody
+    try {
+        let data = req.body
+        let { title, excerpt, userId, ISBN, category, subcategory,releasedAt} = data
 
-        if(!reqBody) return res.status(400).send({status: false , msg: "Book data is required"})
-        if(!title) return res.status(400).send({status: false , msg: "Title is required"})
-        if(!excerpt) return res.status(400).send({status: false, msg: "Excerpt is required"})
-        if(!userId) return res.status(400).send({status: false, msg: "User ID is required"})
-        if(!ISBN) return res.status(400).send({status: false, msg: "ISBN is required"})
-        if(!category) return res.status(400).send({status: false, msg: "category is required"})
-        if(!subcategory) return res.status(400).send({status: false, msg: "Sub category is required"})
-        if(!releasedAt) return res.status(400).send({status: false, msg: "Book release date is required"})
+        if (!checkInputsPresent(data)) return res.status(400).send({ status: false, message: "No data found from body!" })
 
-        const bookData = await bookModel.create(reqBody)
+        if (!checkString(userId)) { return res.status(400).send({ status: false, message: "Please Insert userId." }) }
+        if (!ObjectId.isValid(userId)) { return res.status(400).send({ status: false, message: `This UserId: ${userId} is not Valid.` }) }
+        
+        let checkUserId = await userModel.findById(userId)
+        if (!checkUserId) { return res.status(400).send({ status: false, message: `${userId} is not Exist.` }) }
 
-        return res.status(201).send({status: true , data: bookData})
+        if (!checkString(title)) return res.status(400).send({ status: false, message: "Please Provide Title of Book." })
+        if (!validateTName(title)) return res.status(400).send({ status: false, message: "Invalid Title." });
 
+        if (!checkString(excerpt)) return res.status(400).send({ status: false, message: "Please Provide Book Excerpt." })
+        if (!validateName(excerpt)) return res.status(400).send({ status: false, message: "Invalid Excerpt." });
 
+        if (!checkString(ISBN)) return res.status(400).send({ status: false, message: "Please Provide Book ISBN." })
+        if (!validateISBN(ISBN)) return res.status(400).send({ status: false, message: "Invalid ISBN." });
+
+        if (!checkString(category)) return res.status(400).send({ status: false, message: "Please Provide Book Category." })
+        if (!validateName(category)) return res.status(400).send({ status: false, message: "Invalid Category." });
+
+        if (!checkString(subcategory)) return res.status(400).send({ status: false, message: "Please Provide Book Subcategory." })
+        if (!validateName(subcategory)) return res.status(400).send({ status: false, message: "Invalid Subcategory." });
+
+        if (!checkString(releasedAt)) return res.status(400).send({ status: false, message: "Please Provide Book releasedAt Date." });
+        if (!validateDate(releasedAt)) return res.status(400).send({ status: false, message: "Invalid Date Format. You should use this format (YYYY-MM-DD)" });
+
+        let checkDuplicateTitle = await bookModel.findOne({ title: title })
+        if (checkDuplicateTitle) {
+            return res.status(400).send({ status: false, message: `This Book: ${title} is already exist. Please provide another Title.` })
+        }
+
+        let checkDuplicateISBN = await bookModel.findOne({ ISBN: ISBN })
+        if (checkDuplicateISBN) {
+            return res.status(400).send({ status: false, message: `This ISBN: ${ISBN} is already exist. Please provide another ISBN.` })
+        }
+
+        let createBook = await bookModel.create(data)
+
+        res.status(201).send({ status: true, message: `This ${title} Book is created sucessfully.`, data: createBook })
+
+    } catch (error) {
+
+        res.status(500).send({ status: 'error', error: error.message })
     }
-    catch (err){
-        return res.status(500).send({error: err.message})
-    }
+
 }
+
 
 // =============================get books================================================
 
@@ -146,15 +174,6 @@ const updateBookById = async (req, res) => {
 let deleteBook = async function (req, res) {
     try {
         const bookId = req.params.bookId
-        
-        // if (!validator.checkString((bookId) && isValidObjectId(bookId))) {
-        //     return res.status(400).send({ status: false, msg: "bookId is not valid" })
-        // }
-        // const book = await bookModel.findOne({ _id: bookId })
-        // if (!book) {
-        //     res.status(404).send({ status: false, message: `id don't exist in book collection` })
-        //     return
-        // }
       
        let deletedBook = await bookModel.findOneAndUpdate({ _id: bookId, isDeleted: false, deletedAt: null },
             { isDeleted: true, deletedAt: new Date() }, { new: true })
